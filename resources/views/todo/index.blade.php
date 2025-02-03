919e27a7ee648a8d2b6a26ccf0de883d7afd350f
Franchesto <frandreotti10@gmail.com>
2025-02-03 18:59:26 +0000

    Updated HomePage to display Database

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        table, th, td {
+            border:1px solid black;
+        }
+    </style>
 
     <title>
         Todo List
@@ -12,30 +17,33 @@
 </head>
 <body>
 
-<table class="table">
-    <thead>
-    <tr>
-        <th>Todo ID</th>
-        <th>Todo Name</th>
-        <th>Tasks</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($todos as $todo)
-        <tr>
-            <td>{{ $todo->id }}</td>
-            <td>{{ $todo->name }}</td>
-            <td>
-                <ul>
-                    @foreach ($todo->tasks as $task)
-                        <li>{{ $task->name }}</li>
-                    @endforeach
-                </ul>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+    <div>
+        <h1>ToDo List</h1>
+    </div>
+    <table class="table" style="width:100%">
+        @foreach ($todos as $todo)
+        <thead>
+            <tr>
+                <th>Tasks</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            @foreach ($todo->tasks as $task)
+                <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>{{ $task->status }}</td>
+                    <td>{{ $todo->date }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+        @endforeach
+    </table>
 
 </body>
 </html>
